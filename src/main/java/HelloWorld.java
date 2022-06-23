4f4f44d1a048293b90909f00b9129d27a3a3c8c3
ekanadha <96290319+ekanadha@users.noreply.github.com>
2022-06-23 12:06:08 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "this is anand sparkjava");
+		get("/hello", (req, res) -> "this is ekandha sparkjava");
 	}
 }
